c6c8ab0e1c46f60d5e2a48829911bd9be73ea679
Chris <chrismjboyle@gmail.com>
2020-01-09 11:28:45 +0000

    misc: Tidied up main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
+#include <iostream>
 #include "bignum.hpp"
 
 int main(void)
 {
   BigNum left(10), right(2);
-  //left.print();
-  //right.print();
   BigNum result = left / right;
-  result.print();
+  std::cout << result.repr() << std::endl;
 
 }
